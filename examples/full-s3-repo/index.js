172f96e3587e665132c16246ff75e9f0c938dde2
Jacob Heun <jacobheun@gmail.com>
2018-04-23 18:06:36 +0200

    docs: add s3 lock to the example
    License: MIT
    Signed-off-by: Jacob Heun <jacobheun@gmail.com>

--- examples/full-s3-repo/index.js
+++ examples/full-s3-repo/index.js
@@ -5,8 +5,9 @@ const IPFS = require('ipfs')
 const Repo = require('ipfs-repo')
 const S3 = require('aws-sdk').S3
 const S3Store = require('../../src')
+const S3Lock = require('./s3-lock')
 
-let fileMultihash
+// Initialize the AWS S3 instance
 const s3 = new S3({
   params: {
     Bucket: 'my-bucket'
@@ -15,6 +16,11 @@ const s3 = new S3({
   secretAccessKey: 'mysecretkey'
 })
 
+// Create our custom lock
+const s3Store = new S3Store('/tmp/test/.ipfs', { s3 })
+const s3Lock = new S3Lock(s3Store)
+
+// Create the IPFS Repo, full backed by S3
 const repo = new Repo('/tmp/test/.ipfs', {
   storageBackends: {
     root: S3Store,
@@ -28,34 +34,46 @@ const repo = new Repo('/tmp/test/.ipfs', {
     keys: { s3 },
     datastore: { s3 }
   },
-  lock: 'memory'
+  lock: s3Lock
 })
 
+// Create a new IPFS node with our S3 backed Repo
 let node = new IPFS({
-  repo: repo
+  repo
 })
 
-series([
-  (cb) => node.on('ready', cb),
-  (cb) => node.version((err, version) => {
-    if (err) { return cb(err) }
-    console.log('Version:', version.version)
-    cb()
-  }),
-  (cb) => node.files.add({
-    path: 'hello.txt',
-    content: Buffer.from('Hello World 101')
-  }, (err, filesAdded) => {
-    if (err) { return cb(err) }
-
-    console.log('\nAdded file:', filesAdded[0].path, filesAdded[0].hash)
-    fileMultihash = filesAdded[0].hash
-    cb()
-  }),
-  (cb) => node.files.cat(fileMultihash, (err, data) => {
-    if (err) { return cb(err) }
-
-    console.log('\nFile content:')
-    process.stdout.write(data)
-  })
-])
+// Test out the repo by sending and fetching some data
+node.on('ready', () => {
+  console.log('Ready')
+  node.version()
+    .then((version) => {
+      console.log('Version:', version.version)
+    })
+    // Once we have the version, let's add a file to IPFS
+    .then(() => {
+      return node.files.add({
+        path: 'hello.txt',
+        content: Buffer.from('Hello World 101')
+      })
+    })
+    // Log out the added files metadata and cat the file from IPFS
+    .then((filesAdded) => {
+      console.log('\nAdded file:', filesAdded[0].path, filesAdded[0].hash)
+      return node.files.cat(filesAdded[0].hash)
+    })
+    // Print out the files contents to console
+    .then((data) => {
+      console.log('\nFetched file content:')
+      process.stdout.write(data)
+    })
+    // Log out the error, if there is one
+    .catch((err) => {
+      console.log('File Processing Error:', err)
+    })
+    // After everything is done, shut the node down
+    // We don't need to worry about catching errors here
+    .then(() => {
+      console.log('\n\nStopping the node')
+      return node.stop()
+    })
+})
